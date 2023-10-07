1674ffb1b347a5111b79e770221f0f3302fb224a
joydas <joy600508@gmail.com>
2023-10-08 02:56:17 +0600

    modify my registation form

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -82,7 +82,7 @@ const Register = () => {
 
                         <div>
 
-                            <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="your email" onChange={formik.handleChange} value={formik.values.name} />
+                            <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="your name" onChange={formik.handleChange} value={formik.values.name} />
                             <span className="text-red-600 text-xs">{formik.touched.name ? formik.errors.name : ""}</span>
                         </div>
 
